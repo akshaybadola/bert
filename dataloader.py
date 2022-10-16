Added correct whole_word masking in collator. Also args.optimizer

dataloader.py
@@ -7,6 +7,9 @@ import numpy as np
 from transformers import AutoTokenizer
 import datasets
 
+from common_pyutil.monitor import Timer
+timer = Timer()
+
 
 # TODO: Add seed for numpy maybe
 class BertDataset(torch.utils.data.Dataset):
@@ -33,14 +36,17 @@ class BertDataset(torch.utils.data.Dataset):
 
     """
 
-    def __init__(self, location, shuffle=True, in_memory=False,
-                 max_seq_len=512, min_seq_len=None,
-                 truncate_strategy="proportional"):
-        self.data = datasets.load_from_disk(location, keep_in_memory=in_memory)
+    def __init__(self, location, tokenizer, shuffle=True,
+                 whole_word_mask=True, max_seq_len=512,
+                 min_seq_len=None, truncate_strategy="proportional"):
+        self.data = datasets.load_from_disk(location)
+        self.tokenizer = tokenizer
         self.inds = np.arange(len(self.data))
         self.truncate_strategy = truncate_strategy
         self.max_seq_len = max_seq_len
         self.min_seq_len = min_seq_len
+        self.cls = self.tokenizer.cls_token_id
+        self.sep = self.tokenizer.sep_token_id
         if shuffle:
             print("Shuffling dataset")
             np.random.shuffle(self.inds)
@@ -49,10 +55,7 @@ class BertDataset(torch.utils.data.Dataset):
         np.random.shuffle(self.inds)
 
     def __getitem__(self, i):
-        output = {x: None for x in
-                  ["input_ids", "token_type_ids",
-                   "attention_mask", "special_tokens_mask",
-                   "next_sentence_label"]}
+        output = {}
         if np.random.choice([0, 1]):
             sent_a, sent_b, next_sent =\
                 self.data[int(self.inds[i])], self.data[int(self.inds[i+1])], 1
@@ -64,14 +67,14 @@ class BertDataset(torch.utils.data.Dataset):
         # Since the max sentence length in the dataset is 512, if we
         # concatenate two sentences it becomes > 512. In that case, reduce
         # the lengths according to a strategy
-        max_seq_len = self.max_seq_len
-        if len_a + len_b - 1 > max_seq_len:
+        max_seq_len = self.max_seq_len - 3  # 3 special tokens
+        if len_a + len_b > max_seq_len:
             if self.truncate_strategy == "proportional":
-                limit_a = int(max_seq_len * len_a / (len_a+len_b))+1
+                limit_a = int(max_seq_len * len_a / (len_a+len_b))
                 limit_b = int(max_seq_len * len_b / (len_a+len_b))
             elif self.truncate_strategy == "truncate_second":
                 min_len_b = self.min_seq_len
-                max_len_a = max_seq_len - min_len_b
+                max_len_a = max_seq_len - min(min_len_b, len_b)
                 if len_a > max_len_a:
                     limit_a = max_len_a
                     limit_b = len_b if len_b < min_len_b else min_len_b
@@ -83,20 +86,54 @@ class BertDataset(torch.utils.data.Dataset):
         else:
             limit_a = len_a
             limit_b = len_b
-        sep = sent_a['input_ids'][-1]
-        output["input_ids"] = torch.as_tensor([*sent_a['input_ids'][:limit_a-1], sep,
-                                               *sent_b['input_ids'][1:limit_b-1], sep],
+        if "tokens" in sent_a:
+            words_a = sent_a['tokens'][:limit_a]
+            words_b = sent_b['tokens'][:limit_b]
+        else:
+            words_a = self.tokenizer.tokenize(sent_a['text'])[:limit_a]
+            words_b = self.tokenizer.tokenize(sent_b['text'])[:limit_b]
+        output = {}
+        split_a = []
+        split_b = []
+        i = 1
+        j = 1
+        for x in words_a:
+            if x.startswith("#"):
+                split_a.append(j)
+            else:
+                split_a.append(i)
+                j = i
+                i += 1
+        i = 1
+        j = 1
+        for x in words_b:
+            if x.startswith("#"):
+                split_b.append(j)
+            else:
+                split_b.append(i)
+                j = i
+                i += 1
+        # output['split_mask'] = torch.as_tensor([0, *[1 if x.startswith("#") else 0
+        #                                              for x in words_a],
+        #                                         0, *[1 if x.startswith("#") else 0
+        #                                              for x in words_b], 0])
+        output['split_tokens'] = torch.as_tensor([0, *split_a, 0,
+                                                  *(torch.as_tensor(split_b) + max(split_a)), 0])
+        output['split_range'] = torch.arange(1, output['split_tokens'].max()+1)
+        output["input_ids"] = torch.as_tensor([self.cls,
+                                               *sent_a['input_ids'][:limit_a], self.sep,
+                                               *sent_b['input_ids'][:limit_b], self.sep],
                                               dtype=torch.long)
-        output["token_type_ids"] = torch.as_tensor([*sent_a['token_type_ids'][:limit_a],
-                                                    *[1 for _ in sent_b['token_type_ids'][1:limit_b]]],
-                                                   dtype=torch.long)
-        output["attention_mask"] = torch.ones(len(sent_a['input_ids'][:limit_a])
-                                              + len(sent_b['input_ids'][:limit_b]) - 1,
-                                              dtype=torch.long)
-        output["special_tokens_mask"] = torch.as_tensor([*sent_a['special_tokens_mask'][:limit_a-1], 1,
-                                                         *sent_b['special_tokens_mask'][1:limit_b-1], 1],
-                                                        dtype=torch.long)
+        output["token_type_ids"] = torch.cat((torch.zeros(limit_a+2, dtype=torch.long),
+                                              torch.ones(limit_b+1, dtype=torch.long)))
+        output["attention_mask"] = torch.ones_like(output['input_ids'])
+        output["special_tokens_mask"] = torch.as_tensor(
+            self.tokenizer.get_special_tokens_mask(output['input_ids'],
+                                                   already_has_special_tokens=True),
+            dtype=torch.long)
         output["next_sentence_label"] = next_sent
+        # if output['split_tokens'].any():
+        #     import ipdb; ipdb.set_trace()
         return output
 
     def __len__(self):
@@ -123,6 +160,7 @@ def _mask_tokens(inputs, special_tokens_mask=None, tokenizer=None,
     80% MASK, 10% random, 10% original.
 
     """
+    _inputs = inputs.clone()
     labels = inputs.clone()
     # We sample a few tokens in each sequence for MLM training (with probability
     # `mlm_probability`)
@@ -143,6 +181,39 @@ def _mask_tokens(inputs, special_tokens_mask=None, tokenizer=None,
 
     # 80% of the time, we replace masked input tokens with tokenizer.mask_token
     # ([MASK])
+    indices_replaced = (torch.bernoulli(torch.full(labels.shape, 0.8)).bool() &
+                        masked_indices)
+    _inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(
+        tokenizer.mask_token)
+
+    # 10% of the time, we replace masked input tokens with random word
+    indices_random = (torch.bernoulli(torch.full(labels.shape, 0.5)).bool() &
+                      masked_indices & ~indices_replaced)
+    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    _inputs[indices_random] = random_words[indices_random]
+
+    # The rest of the time (10% of the time) we keep the masked input tokens
+    # unchanged
+    return _inputs, labels
+
+
+def _mask_whole_words(inputs, special_tokens_mask=None,
+                      tokenizer=None, split_tokens=None,
+                      split_range=None, mlm_probability=0.15, ignore_index=-1):
+    inputs = inputs.clone()
+    labels = inputs.clone()
+    # We sample a few tokens in each sequence for MLM training (with probability
+    # `mlm_probability`)
+    pm = torch.full(split_range.shape, mlm_probability)
+    pm[~split_range.bool()] = 0
+    replacements = torch.bernoulli(pm)
+    masked_indices = torch.empty(inputs.shape, dtype=torch.bool)
+    for i in range(len(inputs)):
+        repl_inds = set(torch.where(replacements[i] == 1)[0].tolist())
+        masked_indices[i] = torch.as_tensor([*map(lambda x: x in repl_inds,
+                                                  split_tokens[i].tolist())],
+                                            dtype=bool)
+    labels[~masked_indices] = ignore_index
     indices_replaced = (torch.bernoulli(torch.full(labels.shape, 0.8)).bool() &
                         masked_indices)
     inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(
@@ -255,7 +326,7 @@ def collator_alt(batch, seq_align_len, tokenizer):
     return output
 
 
-def mlm_collator(batch, seq_align_len, tokenizer, pad_full=0):
+def mlm_collator(batch, seq_align_len, tokenizer, pad_full=0, mask_whole_words=False):
     """Collate for MLM task
 
     Args:
@@ -276,22 +347,35 @@ def mlm_collator(batch, seq_align_len, tokenizer, pad_full=0):
     mask_tensor = torch.zeros(size, dtype=torch.long)
     special_tokens_mask_tensor = torch.zeros(size, dtype=torch.long)
     token_type_id_tensor = torch.zeros(size, dtype=torch.long)
+    if mask_whole_words:
+        split_tokens_tensor = torch.zeros(size, dtype=torch.long)
+        split_range_tensor = torch.zeros(size, dtype=torch.long)
     next_sentence_labels = []
     with torch.no_grad():
         for i, x in enumerate(batch):
-            input_tensor[i, :lengths[i]] = x['input_ids']
-            mask_tensor[i, :lengths[i]] = x['attention_mask']
-            special_tokens_mask_tensor[i, :lengths[i]] = x['special_tokens_mask']
-            token_type_id_tensor[i, :lengths[i]] = x['token_type_ids']
+            limit = lengths[i]
+            input_tensor[i, :limit] = x['input_ids']
+            mask_tensor[i, :limit] = x['attention_mask']
+            special_tokens_mask_tensor[i, :limit] = x['special_tokens_mask']
+            token_type_id_tensor[i, :limit] = x['token_type_ids']
+            if mask_whole_words:
+                split_tokens_tensor[i, :limit] = x['split_tokens']
+                split_range_tensor[i, :x['split_range'].max()] = x['split_range']
             next_sentence_labels.append(x['next_sentence_label'])
-        output["input_ids"], output["masked_lm_labels"] = _mask_tokens(
-            input_tensor, tokenizer=tokenizer,
-            special_tokens_mask=special_tokens_mask_tensor)
+        if mask_whole_words:
+            output["input_ids"], output["masked_lm_labels"] = _mask_whole_words(
+                input_tensor, tokenizer=tokenizer,
+                special_tokens_mask=special_tokens_mask_tensor,
+                split_tokens=split_tokens_tensor,
+                split_range=split_range_tensor)
+        else:
+            output["input_ids"], output["masked_lm_labels"] = _mask_tokens(
+                input_tensor, tokenizer=tokenizer,
+                special_tokens_mask=special_tokens_mask_tensor)
         output["attention_mask"] = mask_tensor
         output["token_type_ids"] = token_type_id_tensor
         output["next_sentence_labels"] = torch.as_tensor(next_sentence_labels)
     return output
-
 
 
 def get_wiki_books_loader(batch_size, num_workers, seq_align_len, shuffle=True,
@@ -299,11 +383,15 @@ def get_wiki_books_loader(batch_size, num_workers, seq_align_len, shuffle=True,
                           truncate_stragety=None):
     if max_seq_len is None:
         raise ValueError("max_seq_len cannot be None")
+    mask_whole_words = True
+    if mask_whole_words:
+        print("Whole word masking is on")
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased-whole")
-    collate_fn = partial(mlm_collator, seq_align_len=seq_align_len, tokenizer=tokenizer)
-    data = BertDataset("books-wiki-tokenized", shuffle=shuffle,
-                       max_seq_len=max_seq_len, min_seq_len=min_seq_len,
-                       truncate_strategy=truncate_stragety)
+    collate_fn = partial(mlm_collator, seq_align_len=seq_align_len,
+                         tokenizer=tokenizer, mask_whole_words=mask_whole_words)
+    data = BertDataset("books-wiki-tokenized-with-tokens", tokenizer,
+                       shuffle=shuffle, max_seq_len=max_seq_len,
+                       min_seq_len=min_seq_len, truncate_strategy=truncate_stragety)
     loader = torch.utils.data.DataLoader(data, shuffle=False,
                                          num_workers=num_workers, drop_last=False,
                                          pin_memory=True, batch_size=batch_size,

pretrain_bert.py
@@ -55,10 +55,10 @@ class BertUpdateFunction(SimpleUpdateFunction):
         scaled_loss.backward()
         total = batch["input_ids"].shape[0]
         if self.is_gradient_accumulation_step(kwargs["batch_num"]):
-            if "trainer" in kwargs:
-                kwargs["trainer"].logger.info("Taking backward step")
-            else:
-                print("Taking backward step")
+            # if "trainer" in kwargs:
+            #     kwargs["trainer"].logger.info("Taking backward step")
+            # else:
+            #     print("Taking backward step")
             self._lr_scheduler.step()  # learning rate warmup
             self._grad_scaler.step(optimizer)
             self._grad_scaler.update()
@@ -100,7 +100,7 @@ def get_model_and_config(config_file, sequence_output_is_dense):
     return model, config
 
 
-def get_optimizer(model, devices, warmup_proportion,
+def get_optimizer(optimizer_name, model, devices, warmup_proportion,
                   max_steps, learning_rate, init_loss_scale):
     param_optimizer = list(model.named_parameters())
     no_decay = ['bias', 'gamma', 'beta', 'LayerNorm']
@@ -109,8 +109,14 @@ def get_optimizer(model, devices, warmup_proportion,
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}]
 
-    optimizer = FusedLAMBAMP(optimizer_grouped_parameters,
-                             lr=learning_rate)
+    if optimizer_name == "lamb":
+        optimizer = FusedLAMBAMP(optimizer_grouped_parameters,
+                                 lr=learning_rate)
+    elif optimizer_name == "adam":
+        optimizer = torch.optim.Adam(optimizer_grouped_parameters,
+                                     lr=learning_rate)
+    else:
+        raise ValueError(f"Unknown optmizer {optimizer_name}")
     lr_scheduler = PolyWarmUpScheduler(optimizer,
                                        warmup=warmup_proportion,
                                        total_steps=max_steps,
@@ -136,6 +142,15 @@ def resume_optimizer(optimizer, checkpoint, phase1, phase2_from_scratch,
 
 def load_grad_scaler(self, saved_state):
     self._grad_scaler.load_state_dict(saved_state["grad_scaler"])
+
+
+def save_checkpoint(self, **kwargs):
+    batch_num = kwargs["batch_num"]
+    if not (batch_num+1) % 100000:
+        prefix = f"{batch_num:06}_"
+        save_name = f"{prefix}_{self.checkpoint_name}"
+        self.logger.info(f"Saving to {save_name}")
+        self._save(save_name)
 
 
 def main():
@@ -183,10 +198,17 @@ def main():
                                    shuffle=not args.testing, max_seq_len=max_seq_len,
                                    min_seq_len=30, truncate_stragety="truncate_second")
 
-    optimizer, lr_scheduler, grad_scaler = get_optimizer(model, devices, warmup_proportion,
+    optimizer, lr_scheduler, grad_scaler = get_optimizer(args.optimizer,
+                                                         model, devices, warmup_proportion,
                                                          max_steps, learning_rate,
                                                          args.init_loss_scale)
-    optimizer.setup_fp32_params()
+    if args.optimizer == "lamb":
+        optimizer.setup_fp32_params()
+    elif args.optimizer == "adam":
+        print("Will ignore gradient accumulation steps")
+        args.gradient_accumulation_steps = 1
+    else:
+        raise ValueError(f"Unknown optmizer {args.optimizer}")
 
     num_epochs = math.ceil(max_steps * 256 / len(loader) / loader.batch_size)
     criterion = BertPretrainingCriterion(config.vocab_size,
@@ -198,7 +220,7 @@ def main():
     trainer_params = {"gpus": devices, "cuda": True,
                       "seed": args.seed, "resume": True,
                       "metrics": ["loss", "scaled_loss", "total"], "val_frequency": 1,
-                      "test_frequency": 5, "log_frequency": 1, "max_epochs": num_epochs}
+                      "test_frequency": 5, "log_frequency": 5, "max_epochs": num_epochs}
     trainer_name = "bert_trainer_" + ("phase2" if phase2 else "phase1")
     data = {"name": "books-wiki", "train": loader.dataset}
     trainer = Trainer(trainer_name, trainer_params, optimizer, model, data,
@@ -213,6 +235,7 @@ def main():
     if not any("post_batch_progress" in x for x in desc):
         trainer.add_to_hook_at_end("post_batch_hook", functions.post_batch_progress)
     trainer.add_to_hook_at_end("post_epoch_hook", lambda self: loader.dataset.reset())
+    trainer.add_to_hook_at_end("post_batch_hook", save_checkpoint)
     # TODO:
     # 1. loader shuffle seed for deterministic load and save
     #    BUT if we resume and it should not shuffle from same seed

tests/test_dataloader.py
@@ -5,6 +5,8 @@ from transformers import AutoTokenizer
 import numpy as np
 
 import dataloader
+from common_pyutil.monitor import Timer
+timer = Timer()
 
 
 def test_dataloader(data, tokenizer):
@@ -28,9 +30,31 @@ def test_dataloader(data, tokenizer):
     assert np.all(lengths == lengths[0])
 
 
+def test_dataloader_whole_words(tokenizer):
+    data = dataloader.BertDataset("books-wiki-tokenized-with-tokens", tokenizer,
+                                  shuffle=False,
+                                  max_seq_len=128, min_seq_len=30,
+                                  truncate_strategy="truncate_second")
+    batch_size = 1024
+    collate_fn = partial(dataloader.mlm_collator, seq_align_len=8,
+                         tokenizer=tokenizer, mask_whole_words=True)
+    loader = torch.utils.data.DataLoader(data, shuffle=False,
+                                         num_workers=4, drop_last=False,
+                                         pin_memory=True, batch_size=batch_size,
+                                         collate_fn=collate_fn)
+    iter = loader.__iter__()
+    for _ in range(10):
+        with timer:
+            batch = iter.__next__()
+            assert batch['input_ids'].shape[1] <= 512
+            lengths = np.array([x.shape[1] for x in batch.values() if len(x.shape) == 2])
+            assert np.all(lengths == lengths[0])
+        print(timer.time)
 
-def test_data_128(tokenizer):
+
+def test_data_128_without_tokens(tokenizer):
     data = dataloader.BertDataset("books-wiki-tokenized", shuffle=False,
+                                  whole_word_mask=False,
                                   max_seq_len=128, min_seq_len=30,
                                   truncate_strategy="truncate_second")
     batch_size = 32
@@ -41,3 +65,43 @@ def test_data_128(tokenizer):
     assert collated['input_ids'].shape[1] <= 128
     lengths = np.array([x.shape[1] for x in collated.values() if len(x.shape) == 2])
     assert np.all(lengths == lengths[0])
+
+
+def test_data_128_with_tokens(tokenizer):
+    data = dataloader.BertDataset("books-wiki-tokenized-with_tokens", shuffle=False,
+                                  whole_word_mask=False,
+                                  max_seq_len=128, min_seq_len=30,
+                                  truncate_strategy="truncate_second")
+    batch_size = 32
+    inds = np.arange(len(data))
+    batch_inds = np.random.choice(inds, batch_size)
+    batch = [data[i] for i in batch_inds]
+    collated = dataloader.mlm_collator(batch, 8, tokenizer)
+    assert collated['input_ids'].shape[1] <= 128
+    lengths = np.array([x.shape[1] for x in collated.values() if len(x.shape) == 2])
+    assert np.all(lengths == lengths[0])
+
+
+
+def test_data_whole_word(tokenizer):
+    data = dataloader.BertDataset("books-wiki-tokenized-with-tokens", tokenizer,
+                                  shuffle=False,
+                                  max_seq_len=128, min_seq_len=30,
+                                  truncate_strategy="truncate_second")
+    batch_size = 32
+    inds = np.arange(len(data))
+    batch_inds = np.random.choice(inds, batch_size)
+    batch = [data[i] for i in batch_inds]
+    with timer:
+        collated = dataloader.mlm_collator(batch, 8, tokenizer)
+    print(timer.time)
+    with timer:
+        collated_whole = dataloader.mlm_collator(batch, 8, tokenizer, mask_whole_words=True)
+    print(timer.time)
+    assert collated['input_ids'].shape[1] <= 128
+    lengths = np.array([x.shape[1] for x in collated.values() if len(x.shape) == 2])
+    assert np.all(lengths == lengths[0])
+    assert collated_whole['input_ids'].shape[1] <= 128
+    lengths = np.array([x.shape[1] for x in collated_whole.values() if len(x.shape) == 2])
+    assert np.all(lengths == lengths[0])
+    